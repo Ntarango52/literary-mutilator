e08123286ad33df9c8d53a0b90bf525640223474
Nohemi Tarango <Nohemi5270@gmail.com>
2020-04-21 08:22:50 -0600

    made the literary mutilator file and typed the three paragraphs

---
+++ literary-mutilator/script.js
@@ -0,0 +1,10 @@
+
+function changeFont() {
+	let voyages = document.getElementById("voyages");
+	voyages.style.fontFamily = ("Georgia");
+}
+
+function turnPurple() {
+	let voyages = document.getElementById("voyages");
+	voyages.style.color = "purple";
+}
